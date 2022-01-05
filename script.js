ac3457631671fd24eb88a002205dec3310ebe382
Epshtein Dmitry <letuver@yahoo.com>
2022-01-05 14:53:11 +0200

    remove functions

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
-const getData = (url, itemId) =>{
-    return fetch(url + itemId, {
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    }).then((response) => response.json());
-};
-
-const sendData = (url, parameters) => {
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-            parameters
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch(error => console.log(error));
-};
-
-
-let url = 'https://jsonplaceholder.typicode.com/posts/';
-
-getData(url, 77).then((data) => console.log(data))
-    .catch(error => console.log(error));
-
-
-let parameters = {
-    title: 'Send Data',
-    body: 'request POST',
-    userId: 2,
-};
-sendData(url, parameters);
-
-
-const getData2 = () => {
-    return fetch('db.json', {
+    
+const getData = file => {
+    return fetch(file, {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
     }).then((response) => response.json())
+    .then(data => sendData(data))
     .catch(error => console.log(error));
 };
 
-getData2().then(data => sendData2(data));
-
-const sendData2 = parameters => {
+const sendData = parameters => {
+    let url = 'https://jsonplaceholder.typicode.com/posts/';
+    
     fetch(url, {
         method: 'POST',
         body: JSON.stringify({
@@ -61,3 +25,5 @@ const sendData2 = parameters => {
     .then((json) => console.log(json))
     .catch(error => console.log(error));
 };
+
+getData("db.json");
